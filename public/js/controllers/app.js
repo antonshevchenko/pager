4622efea3d5efdbf28ba9057f6f4a2bf85ad2d89
Alex I <alisterdev@users.noreply.github.com>
2015-05-02 18:26:22 -0400

    added header

--- public/js/controllers/app.js
+++ public/js/controllers/app.js
@@ -4,4 +4,9 @@ angular.module('pager')
   $scope.page = [
     {"about": "About"}
   ];
+})
+
+.controller('HeaderController', function($scope) {
+ 	$scope.user = 'Anton';
+ 	$scope.image = 'https://dl.dropboxusercontent.com/content_link/AuIGRgg5QyV4UKwUd21NVBxd6a0i0Je1BXb5kJXhD4YYJqiOh6e3c9d1prgTueGD';
 });
