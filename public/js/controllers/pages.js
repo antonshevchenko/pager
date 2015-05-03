db753baccb043a156edcdcee5267c4992dcb14e8
Alex I <alisterdev@users.noreply.github.com>
2015-05-02 22:16:19 -0400

    swayyyy

--- public/js/controllers/pages.js
+++ public/js/controllers/pages.js
@@ -1,12 +1,13 @@
 angular.module('pager')
 
 .controller('PagesCtrl', function($scope, $state) {
+  
   $scope.pages = [
     { id: 123, title: 'Hannah Montana', image: 'http://images.livemixtapes.com/artists/blaze/migo_lingo/cover.jpg', likes: 480, checkins: 1245 },
     { id: 456, title: 'Hannah Banana', image: 'http://images.livemixtapes.com/artists/blaze/migo_lingo/cover.jpg', likes: 12480, checkins: 542231 },
   ];
 
-  $scope.editPage = function(page) {
-    $state.go('app.pages.edit', { id: page.id });
+  $scope.editPage = function(page, swag) {
+    $state.go('app.edit', { id: page.id, name: swag});
   };
 });
