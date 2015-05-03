d17bd23a065b234b4921e42045b92eb2f97e8c12
Anton Shevchenko <antonshevchenko@icloud.com>
2015-05-02 22:18:34 -0700

    Load user info for header by default

--- public/js/controllers/app.js
+++ public/js/controllers/app.js
@@ -1,14 +1,21 @@
 angular.module('pager')
 
 .controller('HeaderCtrl', function($rootScope, $scope, $state, User) {
-  // Update user name in header
-  $rootScope.$on('facebook:login', function() {
+  function updateUser() {
     $scope.name = User.getName().split(' ')[0];
     $scope.image = 'http://agencedianeriel.com/photo/Ostiguy-Jeanne-nouv09.jpg';
+  }
+
+  // Update user name in header
+  $rootScope.$on('facebook:login', function() {
+    updateUser();
   });
 
   // Update navigation in header
   $scope.$on('$viewContentLoaded', function() {
     $scope.view = $state.current.title;
   });
+
+  // By default, update user
+  updateUser();
 });
