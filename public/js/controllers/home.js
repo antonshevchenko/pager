2306279e7cb4d585c3f6db3ae8d4fc4d277be7f9
Anton Shevchenko <antonshevchenko@icloud.com>
2015-05-02 21:57:23 -0700

    Added automatic login

--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -1,17 +1,25 @@
 angular.module('pager')
 
 .controller('HomeCtrl', function($scope, $state, $facebook, User) {
-	$scope.login = function() {
-		$facebook.login().then(function(user) {
-      // Store user token
-      User.setUserID(user.authResponse.userID);
-      User.setAccessToken(user.authResponse.accessToken);
+  function doLogin(data) {
+    // Store user token and id
+    User.setUserID(data.authResponse.userID);
+    User.setAccessToken(data.authResponse.accessToken);
 
-      // Go to the pages view
-      $state.go('app.pages');
+    // Go to the pages view
+    $state.go('app.pages');
+  }
+
+	$scope.login = function() {
+		$facebook.login().then(function(response) {
+      doLogin(response)
 		});
 	};
 
   // Check if logged in
-	// getPages();
+	$facebook.getLoginStatus().then(function(response) {
+    if (response.status === 'connected') {
+      doLogin(response);
+    }
+  });
 });
