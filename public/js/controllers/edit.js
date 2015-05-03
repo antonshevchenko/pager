b843cd43d3d5f3a42b4b216d6fdc3edd76358b36
Anton Shevchenko <antonshevchenko@icloud.com>
2015-05-02 22:55:22 -0700

    Minor remix of edit view

--- public/js/controllers/edit.js
+++ public/js/controllers/edit.js
@@ -3,30 +3,11 @@ angular.module('pager')
 .controller('EditCtrl', function($scope, $stateParams, $facebook) {
   // Retrieve page ID
   var pageID = $stateParams.id;
-  
-    function getPageData() {
-	    $facebook.api(pageID).then(       
-	      function(response) {        
-          console.log(response);
-        },
-	      function(err) {});
-	  }
-  
-	getPageData();
-    
-   $scope.onCheckedAbout= function(page) {
-    $scope.page.showAbout = page.showAbout;
-   };
-   
-   $scope.onCheckedNews= function(page) {
-    $scope.page.showBlog = page.showBlog;
-   };
-   
-   $scope.onCheckedEvents = function(page) {
-    $scope.page.showEvents = page.showEvents;
-   };
-   
-   $scope.onCheckedImages = function(page) {
-    $scope.page.showImages = page.showGallery;
-   };
+
+  $scope.options = {
+    about: false,
+    blog: false,
+    events: false,
+    gallery: false
+  };
 });
