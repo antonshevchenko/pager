9d290bff1c2e18f2a9478654e5b2b327fb6e23c2
Darrel-Day Guerrero <ddayy.guerrero@hotmail.com>
2015-05-02 23:44:50 -0400

    template for edit

--- public/js/controllers/pages.js
+++ public/js/controllers/pages.js
@@ -3,11 +3,11 @@ angular.module('pager')
 .controller('PagesCtrl', function($scope, $state) {
   
   $scope.pages = [
-    { id: 123, title: 'Hannah Montana', image: 'http://images.livemixtapes.com/artists/blaze/migo_lingo/cover.jpg', likes: 480, checkins: 1245 },
+    { id: 1429537494015773, title: 'Hannah Montana', image: 'http://images.livemixtapes.com/artists/blaze/migo_lingo/cover.jpg', likes: 480, checkins: 1245 },
     { id: 456, title: 'Hannah Banana', image: 'http://images.livemixtapes.com/artists/blaze/migo_lingo/cover.jpg', likes: 12480, checkins: 542231 },
   ];
 
-  $scope.editPage = function(page, swag) {
-    $state.go('app.edit', { id: page.id, name: swag});
+  $scope.editPage = function(page) {
+    $state.go('app.edit', { id: page.id });
   };
 });
