3da49b58ca45bffa2460855537bceeac2aeed38a
Anton Shevchenko <antonshevchenko@icloud.com>
2015-05-02 21:53:40 -0700

    Updated EditCtrl

--- public/js/controllers/edit.js
+++ public/js/controllers/edit.js
@@ -3,17 +3,14 @@ angular.module('pager')
 .controller('EditCtrl', function($scope, $stateParams, $facebook) {
   // Retrieve page ID
   var pageID = $stateParams.id;
-  
-  console.log("pageID: " + pageID);
-    function getPageData() {
-	    $facebook.api(pageID).then(       // Replace for a legit ID 
-	      function(response) {        
-          $scope.page = { "About": true, "Blog" : true, "Gallery" : true, "Events" : true };
-        },
-	      function(err) {});
-	  }
-  
-	getPageData();
 
-//  $scope.page = {"field1":123, "field2":456, "field3":789, "field4":10};
+  function getPageData() {
+    $facebook.api(pageID)
+      .then(function(response) {
+        $scope.page = response;
+      });
+  }
+
+  // Get page data
+	getPageData();
 });
