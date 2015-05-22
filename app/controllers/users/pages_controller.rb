89416e99071bdd4b74b9613debf13aafb3b035bf
Anton Shevchenko <antonshevchenko@icloud.com>
2015-05-21 21:43:20 -0400

    Added view to show selected user page

--- app/controllers/users/pages_controller.rb
+++ app/controllers/users/pages_controller.rb
@@ -4,4 +4,8 @@ class Users::PagesController < ApplicationController
   def index
     @pages = current_user.facebook.get_object("me/accounts")
   end
+
+  def show
+    @page = current_user.facebook.get_object(params[:id])
+  end
 end
